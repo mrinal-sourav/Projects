08663db4fe7bc620c47e9648594d0884ccfa9251
mrinalsourav87 <amidst.mrnl@gmail.com>
2018-04-27 03:53:57 -0700

    Carom Billiards
    Carom Billiards

---
+++ Carom Billiards Project/Carom Billiards Source/para_simulator.py
@@ -0,0 +1,287 @@
+import pygame
+import random
+import math
+from queue import PriorityQueue
+from keras.models import load_model, Sequential
+from keras.layers import Dense
+import h5py
+import numpy as np
+
+
+# game parameter initialization:
+
+#window size
+display_width =  720 
+display_height = 620
+
+#table boundaries
+top = 10
+right = 400
+left = 10
+bottom = 600
+
+#colors
+black = (0,0,0)
+white = (255,255,255)
+red = (255,0,0)
+green = (0,255,0) 
+blue = (0,0,255)
+dark_green = (30,90,5) 
+
+# to keep score 
+#score_board = {'score': [0, 0], 'player': ['Player-1', 'Player-2']} 
+
+# game initiations:  
+#pygame.init()
+#game_display = pygame.display.set_mode((display_width, display_height))
+#clock = pygame.time.Clock() 
+
+# game physics 
+friction = 0.012
+elasticity = 0.85
+stop_speed = 0.5
+max_speed = 10.0 
+
+# some definitions of angles for initializing data:
+degrees = [x for x in range(271)]
+radians = [math.radians(a) for a in degrees]
+degrees2 = [x for x in range(-89, 0)]
+radians2 = [math.radians(a) for a in degrees2]
+rads =  radians + radians2
+speed_range = [x for x in range(1,11)]
+
+# the ball class 
+class Ball:
+    
+    def __init__(self, size = 50, position = (int(right/2),int(bottom/2)), color = (0,0,0), speed = 0.0, angle = 0):
+        self.size =  size
+        self.x = position[0]
+        self.y = position[1]
+        self.color = color
+        self.speed = speed 
+        self.angle = angle
+        self.selected = False
+        self.a = None
+        self.b = None
+        
+    def modulo_angle(self, angle_in_rad):
+        degrees = math.degrees(angle_in_rad)
+        degrees = degrees % 360 
+        modulated_angle = math.radians(degrees)
+        return modulated_angle
+
+    def move(self):
+        self.x += math.sin(self.angle) * self.speed
+        self.y -= math.cos(self.angle) * self.speed
+        self.speed -= friction 
+        if self.speed < stop_speed:
+            self.speed = 0.0 
+            #self.angle = 0
+        if self.speed > max_speed:
+            self.speed = max_speed
+
+    def bounce(self):
+        if self.x >= (right + left) - self.size:
+            self.x = 2*((right + left) - self.size) - self.x
+            self.angle = self.modulo_angle(-self.angle)
+            self.speed *= elasticity
+
+        elif self.x <= self.size + left:
+            self.x = 2*(self.size + left) - self.x
+            self.angle = self.modulo_angle(-self.angle)
+            self.speed *= elasticity
+
+        if self.y >= (bottom + top) - self.size:
+            self.y = 2*((bottom + top) - self.size) - self.y
+            self.angle = self.modulo_angle(math.pi - self.angle)
+            self.speed *= elasticity
+
+        elif self.y <= self.size + top:
+            self.y = 2*(self.size + top) - self.y
+            self.angle = self.modulo_angle(math.pi - self.angle)
+            self.speed *= elasticity    
+
+# functions for the game
+
+def text_objects(score, font):
+    text_surface = font.render(score, True, black) 
+    return text_surface, text_surface.get_rect()
+    
+def random_pos():
+    x_pos = random.randint(left+20, right-20) 
+    y_pos = random.randint(top+20,bottom-20)
+    return (x_pos,y_pos)
+
+def q_ball_select(q_ball, x, y):
+    if math.hypot(q_ball.x - x, q_ball.y - y) <= q_ball.size:
+        q_ball.selected = True
+        q_ball.color = blue 
+        q_ball.speed = 0
+        q_ball.a = x 
+        q_ball.b = y
+
+def q_ball_unselect(q_ball):
+    q_ball.selected = False
+    q_ball.color = white
+    q_ball.a = None 
+    q_ball.b = None 
+
+def q_ball_setab(q_ball, x, y):
+    q_ball.a = x 
+    q_ball.b = y 
+
+# Collision physics and a_star
+def coord_distance(pos1,pos2):
+    dx = pos1[0] - pos2[0] 
+    dy = pos1[1] - pos2[1]
+    distance = math.hypot(dx, dy)
+    return distance
+
+def collision_check(ball1, ball2):
+    distance = coord_distance((ball1.x,ball1.y), (ball2.x,ball2.y))
+    return distance <= (ball1.size + ball2.size) 
+
+def get_neighbors(pos):
+    x = pos[0]
+    y = pos[1] 
+    neighbors = [(x+1,y), (x-1,y), (x,y+1), (x,y-1),
+                (x+1,y+1), (x+1,y-1), (x-1,y+1), (x-1,y-1)]
+    return neighbors
+
+# a_star is used to search for the closest position ball2 could take
+# to do away with overlap. 
+# returns that position for ball2 
+def a_star(b1,b2, dist): 
+    origin = (b1.x,b1.y)
+    goal_dist = 2*b1.size + 2   # the right distance between balls without overlap (+2 --> to be safe!)
+    error = abs(goal_dist - dist) #the current 'wrong' distance between the balls - absolute is taken to handle relative ball positions
+    start_pos = (b2.x, b2.y)
+    the_q = PriorityQueue(maxsize = 800)
+    the_q.put((error, start_pos)) # initiating the queue
+    while the_q: 
+        neighbors = [] 
+        curr_error, curr_pos = the_q.get() # returns a "less wrong" position to minimize error (priority queue)
+        if curr_error < 1: 
+            return curr_pos 
+        neighbors = get_neighbors(curr_pos)
+        for pos in neighbors:
+            new_dist = coord_distance(origin, pos)
+            new_error = abs(goal_dist - new_dist) 
+            if new_error < 1:
+                return pos 
+            the_q.put((new_error,pos)) #add neighbor to the queue for further search from them
+
+def modulo_angle(angle_in_rad):
+    degrees = math.degrees(angle_in_rad)
+    degrees = degrees % 360 
+    modulated_angle = math.radians(degrees)
+    return modulated_angle
+
+def collide(b1,b2):
+    dx = b1.x - b2.x 
+    dy = b1.y - b2.y 
+
+    tangent = math.atan2(dy, dx)
+    b1.angle = modulo_angle(2*tangent - b1.angle)
+    b2.angle = modulo_angle(2*tangent - b2.angle)
+
+    # adjust overlap using a_star to find the best point without overlap
+    distance = coord_distance((b1.x,b1.y),(b2.x,b2.y)) 
+    new_x_for_b2, new_y_for_b2 = a_star(b1,b2, distance)
+    b2.x = new_x_for_b2
+    b2.y = new_y_for_b2
+
+    if not collision_check(b1,b2):
+        new_speed = elasticity * (b1.speed + b2.speed)/2
+        b1.speed = b2.speed = new_speed
+
+
+def game_loop(no_of_rounds):
+
+    q_ball = Ball(color = white)
+    red_ball = Ball(color = red, position = random_pos(), speed = 5.0)
+    black_ball = Ball(color = black, position = random_pos(), speed = 5.0)
+
+    balls = [q_ball, red_ball, black_ball] 
+
+    stop = False
+    rounds = 0
+    
+    player_score = 0
+    collided_with_red = False
+    collided_with_black = False
+    game_round_start = False
+    reward = 0
+
+    while not stop:
+
+        if q_ball.speed == 0 and red_ball.speed == 0.0 and black_ball.speed == 0.0:
+            collided_with_red = False
+            collided_with_black = False 
+            
+            new_state = [q_ball.x, q_ball.y, red_ball.x, red_ball.y, black_ball.x, black_ball.y]
+            q_ball.angle = random.choice(rads)
+            q_ball.speed = random.choice(speed_range)
+            new_action = [q_ball.angle, q_ball.speed] 
+            state_action = new_state + new_action
+            inputs.append(state_action)
+            
+            game_round_start = True
+            rounds+=1
+            reward = 0
+            if rounds >= no_of_rounds:
+                stop = True
+
+        if collision_check(q_ball, red_ball) and game_round_start:
+            collide(q_ball, red_ball)
+            collided_with_red = True
+            if collided_with_black:
+                player_score += 1000
+                reward = 100
+            else:
+                player_score += 10
+                reward = 10
+                       
+        if collision_check(q_ball, black_ball) and game_round_start:
+            collide(q_ball, black_ball) 
+            collided_with_black = True
+            if collided_with_red:
+                player_score += 1000
+                reward = 100
+            else:
+                player_score += 10
+                reward = 10 
+
+        if collision_check(red_ball, black_ball) and game_round_start:
+            collide(red_ball, black_ball)
+
+        
+        for ball in balls:
+            ball.move()
+            ball.bounce()
+            
+        if q_ball.speed == 0.0 and red_ball.speed == 0.0 and black_ball.speed == 0.0 and game_round_start:
+            targets.append([reward])
+            game_round_start = False
+
+ 
+
+inputs = [] 
+targets = []
+for i in range(3000,4000):
+    #pygame.init()
+    #game_display = pygame.display.set_mode((display_width, display_height))
+    #clock = pygame.time.Clock() 
+    game_loop(i)
+    #pygame.quit()
+
+    if len(inputs)>len(targets):
+        inputs = inputs[:-1]
+        
+    print ('iteration = ', i)
+
+    train_inputs = np.array(inputs) 
+    train_targets = np.array(targets)
+
+    np.save('training_data/train_inputs_para_50', train_inputs)
+    np.save('training_data/train_targets_para_50', train_targets)
